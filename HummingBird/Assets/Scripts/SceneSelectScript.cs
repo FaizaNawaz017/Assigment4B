24ee8be4d3affb8020d99c353771daccd29de91d
FaizaNawaz017 <64863731+FaizaNawaz017@users.noreply.github.com>
2020-07-13 22:01:09 +0500

    done

---
+++ HummingBird/Assets/Scripts/SceneSelectScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectScript : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "Scene01Button":
+                SceneManager.LoadScene("Penguins");
+                break;
+            case "Scene02Button":
+                SceneManager.LoadScene("FlowerIsland");
+                break;
+        }
+    }
+}
